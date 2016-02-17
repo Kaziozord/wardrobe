21e198722a24ae2baa02eb42faf9e27d95bbbfa8
Kazio <mr.coldhand@gmail.com>
2016-02-18 00:31:36 +0100

    add model: Item

---
+++ app/models/item.rb
@@ -0,0 +1,15 @@
+class Item < ActiveRecord::Base
+  has_many :values
+  has_many :properties, through: :values
+  has_and_belongs_to_many :states
+  has_and_belongs_to_many :item_sets
+
+  def get_value(property)
+    self.values.find {|v| v.property_id == property.id }.value
+  end
+
+  def set_value(property, value)
+    self.values.find {|v| v.property_id == property.id }
+      .update_attributes(value: value)
+  end
+end
