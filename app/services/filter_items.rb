19b8d0188727ea141c5c8d4416181f8a3c78e5d3
Kazio <mr.coldhand@gmail.com>
2016-02-18 16:53:41 +0100

    add service: FilterItems

---
+++ app/services/filter_items.rb
@@ -0,0 +1,21 @@
+class FilterItems
+
+  def self.call(all_items, given_states, filter_type)
+    return all_items if given_states.blank?
+
+    Item.fetch_all.map do |item|
+      case filter_type
+      when :strict
+        given_states.map do |state|
+          item.states.map {|s| s.name}.include?(state)
+        end.include?(false) ? nil : item
+      when :any
+        item.states.find do |state|
+          given_states.include?(state.name)
+        end ? item : nil
+      else
+        item
+      end
+    end.compact.map{|i| ItemPresenter.call(i)}
+  end
+end
